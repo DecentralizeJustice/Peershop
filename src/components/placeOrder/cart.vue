4235abec7a8d376ec35f7c435333211f28037af9
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-07-09 14:59:20 -0400

    cart coming along

--- src/components/placeOrder/cart.vue
+++ src/components/placeOrder/cart.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, computed } from 'vue'
-import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
+/* import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue' */
 import { ExclamationCircleIcon } from '@heroicons/vue/20/solid'
 import cartSVG from '@/assets/svg/cart.svg'
 const emit = defineEmits(['next'])
@@ -54,6 +54,48 @@ function addToCart() {
   pendingItemNotes.value = ''
   console.log(cart.value)
 }
+const comments = [
+  {
+    id: 1,
+    name: 'Leslie Alexander',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    content:
+      'Explicabo nihil laborum. Saepe facilis consequuntur in eaque. Consequatur perspiciatis quam. Sed est illo quia. Culpa vitae placeat vitae. Repudiandae sunt exercitationem nihil nisi facilis placeat minima eveniet.',
+    date: '1d ago',
+    dateTime: '2023-03-04T15:54Z',
+  },
+  {
+    id: 2,
+    name: 'Michael Foster',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    content:
+      'Laudantium quidem non et saepe vel sequi accusamus consequatur et. Saepe inventore veniam incidunt cumque et laborum nemo blanditiis rerum. A unde et molestiae autem ad. Architecto dolor ex accusantium maxime cumque laudantium itaque aut perferendis.',
+    date: '2d ago',
+    dateTime: '2023-03-03T14:02Z',
+  },
+  {
+    id: 3,
+    name: 'Dries Vincent',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    content:
+      'Quia animi harum in quis quidem sint. Ipsum dolorem molestias veritatis quis eveniet commodi assumenda temporibus. Dicta ut modi alias nisi. Veniam quia velit et ut. Id quas ducimus reprehenderit veniam fugit amet fugiat ipsum eius. Voluptas nobis earum in in vel corporis nisi.',
+    date: '2d ago',
+    dateTime: '2023-03-03T13:23Z',
+  },
+  {
+    id: 4,
+    name: 'Lindsay Walton',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    content:
+      'Unde dolore exercitationem nobis reprehenderit rerum corporis accusamus. Nemo suscipit temporibus quidem dolorum. Nobis optio quae atque blanditiis aspernatur doloribus sit accusamus. Sunt reiciendis ut corrupti ab debitis dolorem dolorem nam sit. Ducimus nisi qui earum aliquam. Est nam doloribus culpa illum.',
+    date: '3d ago',
+    dateTime: '2023-03-02T21:13Z',
+  },
+]
 
 const allready = computed(() => {
   return true
@@ -126,88 +168,30 @@ const allready = computed(() => {
           </div>
           <div class="w-full md:w-1/2 p-10 text-center">
           <div class="flex flex-col justify-center py-3 h-full bg-gray-800 rounded-3xl">
-            <div class="flex-initial justify-center">
+            <div class="flex-initial justify-center" v-if="cart.length < 1">
               <h2 class="font-heading text-3xl text-white font-black tracking-tight px-4">Your Cart Is Empty</h2>
               <div class="w-1/3 p-4 flex flex-col justify-center mx-auto"><img class="mx-auto" :src="cartSVG" alt=""></div>
             </div>
-          </div>
-        </div>
-<!--         <div class="w-full p-10 text-center">
-          <div class="flex flex-col justify-center py-3 h-full bg-gray-800 rounded-3xl">
-            <form>
-    <div class="space-y-12">
-
-      <div class="p-8">
-        <h2 class="text-base font-semibold leading-7 text-white">Personal Information</h2>
-        <p class="mt-1 text-sm leading-6 text-gray-400">Use a permanent address where you can receive mail.</p>
-
-        <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div class="sm:col-span-3">
-            <label for="first-name" class="block text-sm font-medium leading-6 text-white">First name</label>
-            <div class="mt-2">
-              <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-3">
-            <label for="last-name" class="block text-sm font-medium leading-6 text-white">Last name</label>
-            <div class="mt-2">
-              <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-4">
-            <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
-            <div class="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-3">
-            <label for="country" class="block text-sm font-medium leading-6 text-white">Country</label>
-            <div class="mt-2">
-              <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black">
-                <option>United States</option>
-                <option>Canada</option>
-                <option>Mexico</option>
-              </select>
-            </div>
-          </div>
-
-          <div class="col-span-full">
-            <label for="street-address" class="block text-sm font-medium leading-6 text-white">Street address</label>
-            <div class="mt-2">
-              <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-2 sm:col-start-1">
-            <label for="city" class="block text-sm font-medium leading-6 text-white">City</label>
-            <div class="mt-2">
-              <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-2">
-            <label for="region" class="block text-sm font-medium leading-6 text-white">State / Province</label>
-            <div class="mt-2">
-              <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-2">
-            <label for="postal-code" class="block text-sm font-medium leading-6 text-white">ZIP / Postal code</label>
-            <div class="mt-2">
-              <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
+            <div class="px-10 py-5 text-white" v-if="cart.length > 0">
+              <h2 class="inline-block text-2xl text-blue-500 font-bold uppercase tracking-widest">Your Cart</h2>
+              <ul role="list" class="divide-y divide-gray-100">
+                <li v-for="comment in comments" :key="comment.id" class="flex gap-x-4 py-5">
+                  <img class="h-12 w-12 flex-none rounded-full bg-gray-50" :src="comment.imageUrl" alt="" />
+                  <div class="flex-auto">
+                    <div class="flex items-baseline justify-between gap-x-4">
+                      <p class="text-sm font-semibold leading-6 text-white">{{ comment.name }}</p>
+                      <p class="flex-none text-xs text-white">
+                        <time :datetime="comment.dateTime">{{ comment.date }}</time>
+                      </p>
+                    </div>
+                    <p class="mt-1 line-clamp-2 text-sm leading-6 text-white">{{ comment.content }}</p>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-      </div>
-    </div>
 
-  </form>
-          </div>
-        </div> -->
         
           <div class="w-full p-2 justify center">
           <div class="container py-10 px-10 mx-0 min-w-full flex flex-col items-center">

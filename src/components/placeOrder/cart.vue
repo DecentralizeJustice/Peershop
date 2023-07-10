f8991e624e0d630fc3256b407c6d2eb8b1c9e6c7
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-07-10 12:47:28 -0400

    cart styling

--- src/components/placeOrder/cart.vue
+++ src/components/placeOrder/cart.vue
@@ -5,6 +5,9 @@ import { ExclamationCircleIcon } from '@heroicons/vue/20/solid'
 import cartSVG from '@/assets/svg/cart.svg'
 import box1 from '@/assets/svg/box1.svg'
 import box2 from '@/assets/svg/box2.svg'
+import box3 from '@/assets/svg/box3.svg'
+import box4 from '@/assets/svg/box4.svg'
+import box5 from '@/assets/svg/box5.svg'
 const emit = defineEmits(['next'])
 
 const pendingItemLink = ref('')
@@ -54,57 +57,21 @@ function addToCart() {
   pendingItemPrice.value = 0,
   pendingItemQuantity.value = 1,
   pendingItemNotes.value = ''
-  console.log(cart.value)
 }
 function getbox(index) {
-  if(((index+1) % 2  == 0)) {
-    return box1
-  }
-  return box2
+  const boxlist = [
+    box1,
+    box2,
+    box3,
+    box4,
+    box5
+  ]
+  return boxlist[(index) % boxlist.length]
 }
-const comments = [
-  {
-    id: 1,
-    name: 'Leslie Alexander',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    content:
-      'Explicabo nihil laborum. Saepe facilis consequuntur in eaque. Consequatur perspiciatis quam. Sed est illo quia. Culpa vitae placeat vitae. Repudiandae sunt exercitationem nihil nisi facilis placeat minima eveniet.',
-    date: '1d ago',
-    dateTime: '2023-03-04T15:54Z',
-  },
-  {
-    id: 2,
-    name: 'Michael Foster',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    content:
-      'Laudantium quidem non et saepe vel sequi accusamus consequatur et. Saepe inventore veniam incidunt cumque et laborum nemo blanditiis rerum. A unde et molestiae autem ad. Architecto dolor ex accusantium maxime cumque laudantium itaque aut perferendis.',
-    date: '2d ago',
-    dateTime: '2023-03-03T14:02Z',
-  },
-  {
-    id: 3,
-    name: 'Dries Vincent',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    content:
-      'Quia animi harum in quis quidem sint. Ipsum dolorem molestias veritatis quis eveniet commodi assumenda temporibus. Dicta ut modi alias nisi. Veniam quia velit et ut. Id quas ducimus reprehenderit veniam fugit amet fugiat ipsum eius. Voluptas nobis earum in in vel corporis nisi.',
-    date: '2d ago',
-    dateTime: '2023-03-03T13:23Z',
-  },
-  {
-    id: 4,
-    name: 'Lindsay Walton',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    content:
-      'Unde dolore exercitationem nobis reprehenderit rerum corporis accusamus. Nemo suscipit temporibus quidem dolorum. Nobis optio quae atque blanditiis aspernatur doloribus sit accusamus. Sunt reiciendis ut corrupti ab debitis dolorem dolorem nam sit. Ducimus nisi qui earum aliquam. Est nam doloribus culpa illum.',
-    date: '3d ago',
-    dateTime: '2023-03-02T21:13Z',
-  },
-]
-
+function removeItem(index) {
+  cart.value = cart.value.splice(index + 1, 1)
+  return
+}
 const allready = computed(() => {
   return true
 })
@@ -175,24 +142,36 @@ const allready = computed(() => {
             </div>
           </div>
           <div class="w-full md:w-1/2 p-10 text-center">
-          <div class="flex flex-col justify-center py-3 h-full bg-gray-800 rounded-3xl">
+          <div class="flex flex-col justify-center h-full bg-gray-800 rounded-3xl">
             <div class="flex-initial justify-center" v-if="cart.length < 1">
-              <h2 class="font-heading text-3xl text-white font-black tracking-tight px-4">Your Cart Is Empty</h2>
+              <h2 class="font-heading text-3xl text-white font-black tracking-tight p-4">Your Cart Is Empty</h2>
               <div class="w-1/3 p-4 flex flex-col justify-center mx-auto"><img class="mx-auto" :src="cartSVG" alt=""></div>
             </div>
-            <div class="px-10 py-5 text-white" v-if="cart.length > 0">
+            <div class="px-4 md:px-8 py-5 text-white" v-if="cart.length > 0">
               <h2 class="inline-block text-2xl text-blue-500 font-bold uppercase tracking-widest">Your Cart</h2>
               <ul role="list" class="divide-y divide-gray-100">
-                <li v-for="(comment,index) in comments" :key="comment.id" class="flex gap-x-4 py-5">
+                <li v-for="(item ,index) in cart" :key="item.itemLink" class="flex gap-x-4 py-5">
                   <img class="h-12 w-12 flex-none" :src="getbox(index)" alt="" />
-                  <div class="flex-auto">
+                  <div class="flex-auto md:space-y-2 space-y-4">
                     <div class="flex items-baseline justify-between gap-x-4">
-                      <p class="text-sm font-semibold leading-6 text-white">Item: {{ index + 1 }}</p>
-                      <p class="flex-none text-xs text-white">
-                        <time :datetime="comment.dateTime">{{ comment.date }}</time>
+                      <p class="text-sm font-semibold leading-6 text-white">Item# {{ index + 1 }}</p>
+                      <p class="text-md text-white" v-if="item.quantity < 2">
+                        Item Price:  <br/>{{ item.price }} USD
+                      </p>
+                      <p class="text-md text-white" v-if="item.quantity > 1">
+                        Items Total: <br/>{{ Number(item.price*item.quantity).toFixed(2) }} USD
                       </p>
                     </div>
-                    <p class="mt-1 line-clamp-2 text-sm leading-6 text-white">{{ comment.content }}</p>
+                    <p class="mt-1 line-clamp-2 text-md leading-6 text-white text-left">Quantity: {{ item.quantity }}</p>
+                    <p class="mt-1 text-md leading-6 text-white text-left" v-if="item.quantity > 1">
+                        Item Price: {{ item.price }} USD
+                      </p>
+                    <p class="mt-1 line-clamp-2 text-md leading-6 text-white text-left text-blue-500"><a :href="item.itemLink" target="_blank" rel="noopener noreferrer"
+                      class="text-blue-500 dark:text-blue-500 hover:underline">Product Link:</a></p>
+                      <p  class="mt-1 line-clamp-2 text-md leading-6 text-white text-left break-all">{{ item.itemLink }}</p>
+                    <p v-if='item.notes.length > 0' class="mt-1  text-md leading-6 text-white text-left break-all">Item Notes: <br/>{{ item.notes }}</p>
+                    <button class="md:w-1/4  block w-full pb-1 mt-4 text-lg text-center text-white font-bold bg-red-500  rounded-full"
+                    @click="removeItem(index)">Remove</button>
                   </div>
                 </li>
               </ul>
@@ -209,10 +188,4 @@ const allready = computed(() => {
 </template>
 
 <style scoped>
-.bg-gradient-radial-dark-light {
-  background-image:radial-gradient(50% 50% at 50% 50%,rgba(31,41,55,0) 0,#1f2937 100%)
-}
-.bg-gradient-radial-dark {
-  background-image:radial-gradient(50% 50% at 50% 50%,rgba(17,24,39,0) 0,#111827 100%)
-}
 </style>

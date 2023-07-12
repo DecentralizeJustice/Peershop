6c960f1e600df5577cd46633616941c3140ff2ae
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-07-11 23:14:25 -0400

    finished touch

--- src/components/placeOrder/summary.vue
+++ src/components/placeOrder/summary.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 import globalJson from '@/assets/globalInfo.json'
 import box1 from '@/assets/svg/box1.svg'
 import box2 from '@/assets/svg/box2.svg'
@@ -9,9 +9,11 @@ import box5 from '@/assets/svg/box5.svg'
 const emit = defineEmits(['back', 'goTo'])
 const props = defineProps(['cart', 'lockerInfo'])
 
+const defaultEarnerPercentage = 3
 const lockerInfo = ref({})
 const cart = ref([])
-
+const orderEarnerInfoExpanded = ref(false)
+const earnerPercentage = ref(defaultEarnerPercentage)
 function goTo(step){
   emit('goTo', step)
 }
@@ -25,9 +27,6 @@ function getbox(index) {
   ]
   return boxlist[(index) % boxlist.length]
 }
-const allready = computed(() => {
-  return false
-})
 
 const itemSubtotal = computed(() => {
   let subtotal = Number(0)
@@ -39,14 +38,28 @@ const itemSubtotal = computed(() => {
 const orderTax = computed(() => {
   return itemSubtotal.value*Number(globalJson.estimatedTax)*Number(1/100)
 })
+const costAfterIncentive = computed(() => {
+  if(earnerPercentage.value === ''){
+    return Number.NaN
+  }
+  const subtotal = Number(orderTax.value) + Number(itemSubtotal.value)
+  const discountAmount = Number(subtotal* (Number(earnerPercentage.value) * Number(1/100)))
+  return subtotal + discountAmount
+})
 onMounted(() => {
   cart.value = props.cart
   lockerInfo.value = props.lockerInfo
 })
+watch(earnerPercentage, (percentage) => {
+  if (percentage === '') {
+    return
+  }
+  earnerPercentage.value = Number(parseInt(percentage).toFixed(0))
+})
 </script>
 
 <template>
-          <div class="w-full text-center text-3xl mt-6 text-blue-300 font-bold"><h1>Order Summary</h1></div>
+          <div class="w-full text-center text-3xl mt-6 text-blue-300 font-bold "><h1>Order Summary</h1></div>
           <div class="w-full md:w-1/2 p-10 text-center">
           <div class="flex flex-col justify-content:start h-full bg-gray-800 rounded-3xl max-h-[36rem] overflow-y-auto mt-6">
             <div class="px-4 md:px-8 py-5 text-white">
@@ -101,14 +114,67 @@ onMounted(() => {
         </div>
         <div class="w-full px-2 justify-center mx-auto text-center">
           <div class="px-4 sm:px-0 text-white">
+          <div class="relative  mx-auto">
+        <div class="flex flex-wrap mt-1">
+          <div class="md:w-1/2 p-3 my-3 mx-auto" v-if="!orderEarnerInfoExpanded">
+            <a class="block p-10 bg-gray-800 rounded-3xl text-left" @click="orderEarnerInfoExpanded = true" >
+            <div class="flex flex-wrap -m-2">
+              <div class="flex-1 p-2">
+                <h3 class="font-heading text-xl text-white font-black">What is the Earner Incentive Percentage?</h3>
+              </div>
+              <div class="w-auto p-2">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M17.9207 8.18018H11.6907H6.08072C5.12072 8.18018 4.64073 9.34018 5.32073 10.0202L10.5007 15.2002C11.3307 16.0302 12.6807 16.0302 13.5107 15.2002L15.4807 13.2302L18.6907 10.0202C19.3607 9.34018 18.8807 8.18018 17.9207 8.18018Z" fill="#374151"></path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="w-1/2 p-3 my-3" v-if="orderEarnerInfoExpanded">
+            <a class="block p-10 bg-gray-800 rounded-3xl text-left" @click="orderEarnerInfoExpanded = false">
+            <div class="flex flex-wrap -m-2">
+              <div class="flex-1 p-2">
+                <h3 class="font-heading mb-4 text-xl text-white font-black">What is the Earner Incentive Percentage?</h3>
+                <p class="text-gray-400 font-bold text-lg">The earner is the person who pickups up and completes your order. 
+                  The percentage that you pay them will affect the quality and speed of your order completion. 
+                  You are free to place an order with a large discount but there is no guarantee that your order will be picked up by an earner. You will pay the
+                  Non-Refundable Service Fee if you place an order, regardless of if your order is picked up or not. If you want your order completed reasonably, stick with the default minimum {{  defaultEarnerPercentage }}%.
+                </p>
+              </div>
+              <div class="w-auto p-2">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M6.07928 15.8198L12.3093 15.8198L17.9193 15.8198C18.8793 15.8198 19.3593 14.6598 18.6793 13.9798L13.4993 8.79983C12.6693 7.96983 11.3193 7.96983 10.4893 8.79983L8.51928 10.7698L5.30927 13.9798C4.63927 14.6598 5.11928 15.8198 6.07928 15.8198Z" fill="#374151"></path>
+                </svg>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="md:w-1/2 p-3 my-3 mx-auto">
+            <h3 class="text-2xl">Earner Incentive Percentage (%):</h3>
+            <div class="w-1/2 md:w-1/4 mx-auto md:mt-3 mt-5">
+              <div class="relative mt-2 rounded-md shadow-sm">
+                <div class="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-blue-500">
+                  <input maxlength="20"
+                  v-model="earnerPercentage" type="number" class="flex-1 border-0 bg-transparent py-1.5 pl-1 text-white focus:ring-0 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+            </div>
+        </div>
+        </div>
+      </div>
+        </div>
+        </div>
+        <div class="w-full px-2 pt-6 justify-center mx-auto text-center">
+          <div class="px-4 sm:px-0 text-white">
           <h3 class="text-2xl">Estimated Fees</h3>
 
           <div class="grid md:grid-cols-2 gap-4 md:w-4/6 mx-auto text-xl  text-center md:mt-0 mt-5">
             <div class="md:py-4">
               <div class="text-white">
-                <p class=" mb-4"><span class="text-blue-300 font-bold">Item Subtotal:</span><br/>{{itemSubtotal.toFixed(2)}} USD</p>
+                <p class=" mb-4"><span class="text-blue-300 font-bold">Original Item Subtotal:</span><br/>{{itemSubtotal.toFixed(2)}} USD</p>
                 <p><span class="text-blue-300 font-bold">Estimated {{globalJson.estimatedTax}}% Tax: <br/></span>
                   {{orderTax.toFixed(2)}} USD </p>
+                  <p class=" mt-4"><span class="text-blue-300 font-bold">Items Cost After Incentive:</span><br/>{{costAfterIncentive.toFixed(2)}} USD</p>
                </div>
             </div>
             <div class="md:py-4">
@@ -120,15 +186,14 @@ onMounted(() => {
             </div>
           <div>
             <p class="text-blue-600 font-bold text-3xl mb-1 mt-6 md:mt-3">Order Total:</p>
-            <p  class="text-2xl">{{(itemSubtotal+orderTax+Number(globalJson.myServiceFeeBase)+ Number(globalJson.shopperBond)).toFixed(2)}} USD</p></div>
+            <p  class="text-2xl">~{{(Number(costAfterIncentive)+Number(globalJson.myServiceFeeBase)+ Number(globalJson.shopperBond)).toFixed(2)}} USD</p></div>
         </div>
-
         </div>
           <div class="md:w-2/3 mx-auto">
           <div class="container py-10 px-10 flex flex-col items-center grid md:grid-cols-2 gap-12 ">
             <button  class="mx-auto block w-full px-4 py-2.5 text-lg text-center text-white font-bold bg-red-500 hover:bg-red-600  rounded-full" @click="goTo(2)">Back To Locker Info</button>
-            <button v-if="!allready" disabled class=" mx-auto  block w-full px-4 py-2.5 text-lg text-center text-white font-bold bg-slate-500  rounded-full">Min. Order is {{ globalJson.minLockerOrder }} USD</button>
-            <button v-if="allready"  class=" mx-auto  block w-full px-4 py-2.5 text-lg text-center text-white font-bold bg-green-600  rounded-full" @click="next()">Continue To Payment</button>
+            <button v-if="itemSubtotal < Number(globalJson.minLockerOrder) ||  isNaN(costAfterIncentive)" disabled class=" mx-auto  block w-full px-4 py-2.5 text-lg text-center text-white font-bold bg-slate-500  rounded-full">Min. Order is {{ globalJson.minLockerOrder }} USD</button>
+            <button v-if="!(itemSubtotal < Number(globalJson.minLockerOrder)) && !isNaN(costAfterIncentive)"  class=" mx-auto  block w-full px-4 py-2.5 text-lg text-center text-white font-bold bg-green-600  rounded-full" @click="next()">Continue To Payment</button>
           </div>
         </div>
 

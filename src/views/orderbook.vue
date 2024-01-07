48fcbca38038067bcd39d5e14e2ba3aa28f19242
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2024-01-07 01:57:51 -0500

    basic orderbook

--- src/views/orderbook.vue
+++ src/views/orderbook.vue
@@ -7,52 +7,8 @@ const statuses = {
   'In progress': 'text-gray-600 bg-gray-50 ring-gray-500/10',
   Archived: 'text-yellow-800 bg-yellow-50 ring-yellow-600/20',
 }
-const projects = [
-  {
-    id: 1,
-    name: 'GraphQL API',
-    href: '#',
-    status: 'Complete',
-    createdBy: 'Leslie Alexander',
-    dueDate: 'March 17, 2023',
-    dueDateTime: '2023-03-17T00:00Z',
-  },
-  {
-    id: 2,
-    name: 'New benefits plan',
-    href: '#',
-    status: 'In progress',
-    createdBy: 'Leslie Alexander',
-    dueDate: 'May 5, 2023',
-    dueDateTime: '2023-05-05T00:00Z',
-  },
-  {
-    id: 3,
-    name: 'Onboarding emails',
-    href: '#',
-    status: 'In progress',
-    createdBy: 'Courtney Henry',
-    dueDate: 'May 25, 2023',
-    dueDateTime: '2023-05-25T00:00Z',
-  },
-  {
-    id: 4,
-    name: 'iOS app',
-    href: '#',
-    status: 'In progress',
-    createdBy: 'Leonard Krasner',
-    dueDate: 'June 7, 2023',
-    dueDateTime: '2023-06-07T00:00Z',
-  },
-  {
-    id: 5,
-    name: 'Marketing site redesign',
-    href: '#',
-    status: 'Archived',
-    createdBy: 'Courtney Henry',
-    dueDate: 'June 10, 2023',
-    dueDateTime: '2023-06-10T00:00Z',
-  },
+const people = [
+{ name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
 ]
 </script>
 
@@ -68,53 +24,55 @@ const projects = [
         <h2 class="font-heading mb-6 text-4xl md:text-5xl lg:text-6xl text-white font-black tracking-tight">Order Book</h2>
       </div>
       <ul role="list" class="divide-y divide-gray-100">
-    <li v-for="project in projects" :key="project.id" class="flex items-center justify-between gap-x-6 py-5">
-      <div class="min-w-0">
-        <div class="flex items-start gap-x-3">
-          <p class="text-sm font-semibold leading-6 text-gray-900">{{ project.name }}</p>
-          <p :class="[statuses[project.status], 'rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset']">{{ project.status }}</p>
-        </div>
-        <div class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
-          <p class="whitespace-nowrap">
-            Due on <time :datetime="project.dueDateTime">{{ project.dueDate }}</time>
-          </p>
-          <svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current">
-            <circle cx="1" cy="1" r="1" />
-          </svg>
-          <p class="truncate">Created by {{ project.createdBy }}</p>
+        <div class="bg-gray-900">
+    <div class="mx-auto max-w-7xl">
+      <div class="bg-gray-900 py-10">
+        <div class="px-4 sm:px-6 lg:px-8">
+          <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+              <p class="mt-2 text-sm text-gray-300">A list of all the orders waiting to be fulfilled by shoppers.</p>
+            </div>
+            <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+              <button type="button" class="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Place Order</button>
+            </div>
+          </div>
+          <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <table class="min-w-full divide-y divide-gray-700">
+                  <thead>
+                    <tr>
+                      <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">Name</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">Title</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">Email</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">Role</th>
+                      <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                        <span class="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-800">
+                    <tr v-for="person in people" :key="person.email">
+                      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{ person.name }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{{ person.title }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{{ person.email }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{{ person.role }}</td>
+                      <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                        <a href="#" class="text-indigo-400 hover:text-indigo-300"
+                          >Edit<span class="sr-only">, {{ person.name }}</span></a
+                        >
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="flex flex-none items-center gap-x-4">
-        <a :href="project.href" class="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
-          >View project<span class="sr-only">, {{ project.name }}</span></a
-        >
-        <Menu as="div" class="relative flex-none">
-          <MenuButton class="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-            <span class="sr-only">Open options</span>
-            <EllipsisVerticalIcon class="h-5 w-5" aria-hidden="true" />
-          </MenuButton>
-          <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-            <MenuItems class="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-              <MenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']"
-                  >Edit<span class="sr-only">, {{ project.name }}</span></a
-                >
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']"
-                  >Move<span class="sr-only">, {{ project.name }}</span></a
-                >
-              </MenuItem>
-              <MenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']"
-                  >Delete<span class="sr-only">, {{ project.name }}</span></a
-                >
-              </MenuItem>
-            </MenuItems>
-          </transition>
-        </Menu>
-      </div>
-    </li>
+    </div>
+  </div>
+
   </ul>
     </div>
   </div>

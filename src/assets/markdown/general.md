d72c510580dd5f7e10c02707669714269cbc71a4
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2024-02-26 15:12:33 -0500

    gen order process

--- src/assets/markdown/general.md
+++ src/assets/markdown/general.md
@@ -1,4 +1,27 @@
 # General Guide to Peer Shop
 
-## Introduction
+Peer Shop uses a mixture of [DKIM-signed](https://en.wikipedia.org/wiki/DomainKeys_Identified_Mail) email messages and [zk-proofs](https://tlsnotary.org/) to create a low-cost low-fraud fiat-crypto gateway. Currently, we only support Amazon orders, but plan to expand into other retailers such as Walmart, Target, Mcdonalds etc. 
+
+## Information Directory
+
+This guide only covers the general protections and processes offered on Peer Shop. These resources offer more details on different parts of the platform:
+
+- [Lite Paper](https://peershop.app/litepaper) - An overview of zk proofs for fraud and regulatory resistant p2p platforms.
+- [Shopper Guide](https://peershop.app/guides/shopper) - A detailed overview of shopper expections and requirements on Peer Shop.
+- [Eaner Guide](https://peershop.app/guides/earner) - A detailed overview ofearner expections and requirements on Peer Shop.
+
+The rest of this guide will only offer a quick overview of Peer Shop. Reference the above resources for more details.
+
+## Platform Overview
+Our platform treats amazon.com web data as a trusted oracle. This means that we treat all data from Amazon as being trustworthy and use it to handle disputes and reduce fraud. In cases of disputes, we require a double or nothing style bond to be paid to reduce the abuse of our dispute process. Then the earner and shopper are required to use tls-notary chrome extension to prove certain data about the order. They might also be required to submit signed emails as more proof.
+The detailed process for disputes can be found in the shopper and earner guide.
+
+## General Order Process
+1. First item
+2. Second item
+3. Third item
+
+## Protections Offered to Shoppers on 900+ USD orders
+
+On large orders the earner is required to provide extra proof that they have used the same card on an order that is atleast 2 months old. This will reduce fraud since it requires the shopper to have control of an account/card for at least 2 months.
 
--- src/views/guides.vue
+++ src/views/guides.vue
@@ -33,6 +33,16 @@ function selectQuestion(chosen) {
         <div class="flex flex-wrap -m-4 mb-6">
           <div class="w-full md:w-1/3 p-4">
             <div class="h-full bg-gray-800 rounded-3xl">
+              <img class="w-full rounded-3xl" src="https://res.cloudinary.com/dylevfpbl/image/upload/v1708381901/landingpage/dgoon21_a_monero_branded_vending_machine_8ab87c64-cd63-4a98-856d-1fee614448ad.png" alt="">
+              <div class="p-8 text-center">
+                <a class="group inline-block mb-4 text-center" href="/guides/general">
+                  <h3 class="font-heading text-2xl text-white hover:text-blue-600 group-hover:underline font-black text-center">General Guide</h3>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="w-full md:w-1/3 p-4">
+            <div class="h-full bg-gray-800 rounded-3xl">
               <img class="w-full rounded-3xl" src="https://res.cloudinary.com/dylevfpbl/image/upload/v1708380200/landingpage/0_2.webp" alt="">
               <div class="p-8 text-center">
                 <a class="group inline-block mb-4" href="/guides/shopper">
@@ -51,16 +61,7 @@ function selectQuestion(chosen) {
               </div>
             </div>
           </div>
-          <div class="w-full md:w-1/3 p-4">
-            <div class="h-full bg-gray-800 rounded-3xl">
-              <img class="w-full rounded-3xl" src="https://res.cloudinary.com/dylevfpbl/image/upload/v1708381901/landingpage/dgoon21_a_monero_branded_vending_machine_8ab87c64-cd63-4a98-856d-1fee614448ad.png" alt="">
-              <div class="p-8 text-center">
-                <a class="group inline-block mb-4 text-center" href="/guides/general">
-                  <h3 class="font-heading text-2xl text-white hover:text-blue-600 group-hover:underline font-black text-center">General Guide</h3>
-                </a>
-              </div>
-            </div>
-          </div>
+
         </div>
 <!--         <div class="flex flex-wrap md:justify-center -m-2">
           <div class="w-full md:w-auto p-2"><a class="block w-full px-12 py-3.5 text-lg text-center text-gray-900 font-bold bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:ring-gray-600 rounded-full" href="#">Read more News</a></div>

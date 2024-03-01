e2767b5e9a4144abe1e95c7468655feeacbfb876
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2024-03-01 17:43:20 -0500

    mobile responsive

--- src/views/orderbook.vue
+++ src/views/orderbook.vue
@@ -1,20 +1,20 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { EllipsisVerticalIcon } from '@heroicons/vue/20/solid'
+import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { CheckIcon } from '@heroicons/vue/24/outline'
 import axios from 'axios';
-const statuses = {
-  Complete: 'text-green-700 bg-green-50 ring-green-600/20',
-  'In progress': 'text-gray-600 bg-gray-50 ring-gray-500/10',
-  Archived: 'text-yellow-800 bg-yellow-50 ring-yellow-600/20',
-}
-const people = [
-{ name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-]
+import { RouterLink } from 'vue-router';
+const open = ref(false)
+const chosenOrder = ref(38)
 const orders= ref([])
 async function getOrders() {
   const results = await axios.get('/.netlify/functions/getOrderBook')
   orders.value = results.data
+}
+async function setDialogContent(order) {
+  chosenOrder.value = order
+  open.value = true
+  console.log(order)
 }
 onMounted(() => {
   getOrders()
@@ -47,7 +47,65 @@ onMounted(() => {
           </div>
           <div class="mt-8 flow-root">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+
+
+              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 md:max-2xl:hidden">
+                <table class="min-w-full divide-y divide-gray-700">
+                  <thead>
+                    <tr>
+                      <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0">~USD/XMR</th>
+                      <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">~XMR You Will Recieve</th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-800" >
+                    <tr v-for="(order, index) in orders" :key="order.index">
+                      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{ order.rate }}</td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{{ (Number(order.usd) / Number(order.rate)).toFixed(5) }}</td>
+                      <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                        <button class="text-blue-400 hover:text-blue-300" @click="setDialogContent(index)"
+                          >More Info</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <TransitionRoot as="template" :show="open">
+                <Dialog as="div" class="relative z-10" @close="open = false">
+                  <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
+                    <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                  </TransitionChild>
+
+                  <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                      <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+
+                        <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                          <div>
+                            <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                              <CheckIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+                            </div>
+                            <div class="mt-3 text-center sm:mt-5">
+                              <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Order Info</DialogTitle>
+                              <div class="mt-2 text-xl text-gray-500 text-left">
+                                <p class="">~USD/XMR: {{ orders[chosenOrder].rate  }}</p>
+                                <p class="">~Cost To You(USD): {{ orders[chosenOrder].usd}}</p>
+                                <p class="">~XMR You Will Receive: {{ (Number(orders[chosenOrder].usd) / Number(orders[chosenOrder].rate)).toFixed(5)}}</p>
+                                <p class="">Type: Gift Registry</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="mt-5 sm:mt-6">
+                            <routerLink  class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" :to="`/pickuporder#${orders[chosenOrder].id}`">Pickup Order</routerLink>
+                          </div>
+                        </DialogPanel>
+                      </TransitionChild>
+                    </div>
+                  </div>
+                </Dialog>
+              </TransitionRoot>
+
+
+              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 hidden md:block">
                 <table class="min-w-full divide-y divide-gray-700">
                   <thead>
                     <tr>
@@ -57,7 +115,7 @@ onMounted(() => {
                       <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-white">Type</th>
                     </tr>
                   </thead>
-                  <tbody class="divide-y divide-gray-800">
+                  <tbody class="divide-y divide-gray-800 " >
                     <tr v-for="order in orders" :key="order.index">
                       <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">{{ order.rate }}</td>
                       <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">{{ order.usd }}</td>

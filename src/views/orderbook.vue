8833a64e08d12ec15191ccd96b89fc8f6742abe7
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2024-03-01 17:44:59 -0500

    typo

--- src/views/orderbook.vue
+++ src/views/orderbook.vue
@@ -49,7 +49,7 @@ onMounted(() => {
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 
 
-              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 md:max-2xl:hidden">
+              <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8 md:hidden">
                 <table class="min-w-full divide-y divide-gray-700">
                   <thead>
                     <tr>
